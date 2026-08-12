31d5fd2a20c99b0f40ba066c632a7fcec0dc7027
Mohanadqaid1 <mohanad.qaid@tkmind.net>
2026-08-12 18:13:22 +0300

    feat: register execution router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,12 +1,14 @@
 import { appRouter, mergeRouters, router } from "./index";
 import { strategyRouter } from "./strategy";
 import { scorecardRouter } from "./scorecard";
+import { executionRouter } from "./execution";
 
 export const rootRouter = mergeRouters(
   appRouter,
   router({
     strategy: strategyRouter,
     scorecard: scorecardRouter,
+    execution: executionRouter,
   }),
 );
 
